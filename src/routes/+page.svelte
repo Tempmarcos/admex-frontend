32ae717174f15cbf95a9b6783af7e119a3695c30
Tempmarcos <kikitotemp14@gmail.com>
2025-04-23 11:47:44 -0300

    base p frontend

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Counter from "./Counter.svelte";
+</script>
+
+<svelte:head>
+  <title>Página Inicial</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section></section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+</style>
